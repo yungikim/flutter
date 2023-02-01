7ee0ecb69132662b6b0a9d3224541ed4f67ff173
dosa777 <=>
2023-02-01 13:45:22 +0900

    Modify

--- TestDs/src/com/lgcns/ucap/core/util/LogUtil.java
+++ TestDs/src/com/lgcns/ucap/core/util/LogUtil.java
@@ -3,7 +3,7 @@ package com.lgcns.ucap.core.util;
 public class LogUtil {
 
 	public static void e(String arg1, Exception e) {
-		
+		System.out.println("테스트");
 	}
 	
 	public static void d(String...args) {
